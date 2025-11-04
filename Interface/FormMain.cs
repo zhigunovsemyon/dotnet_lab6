abc09132f701ae5ef6d012581a516173f2b71db8
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-11-04 21:53:04 +0300

    Добавил обёртку для вызова окна запроса на удаление

--- Interface/FormMain.cs
+++ Interface/FormMain.cs
@@ -9,4 +9,10 @@ public partial class FormMain : Form
 
 	/// <summary> Метод для закрытия приложения через пункт меню </summary>
 	private void CloseButton_Click (object sender, EventArgs e) => this.Close();
+
+	/// <summary> Запрос подтверждения от пользователя удаления </summary>
+	/// <param name="text">Текст в окне</param>
+	/// <returns>Ответ пользователя</returns>
+	private static DialogResult VerifyDeletion (string item)
+		=> MessageBox.Show($"Удалить {item}?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 }
